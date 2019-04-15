c06d7dca761443b6560b024eba85b9e2e043cab4
Michael Pratt <staticextasy@gmail.com>
2019-04-15 17:06:17 -0400

    function spanScan

--- dmhscrape.py
+++ dmhscrape.py
@@ -5,38 +5,28 @@ from bs4 import BeautifulSoup
 
 
 dirPath = os.getcwd() #Get the Current working directory
-filePath = "CommerceC\Members -.html" #Specify the File to Scrape
+filePath = "CommerceC/Members -.html" #Specify the File to Scrape
 absFilePath = os.path.join(dirPath, filePath) #Join directory and file to create URL
-print(absFilePath)
+#print(absFilePath)
 
 #Open HTML File to scrape
 page = urlopen("file:///" + absFilePath)
 soup = BeautifulSoup(page, 'html.parser')
-
-def soupFinder(className):
+	
+def spanScan():
 	if page:
-		dataText = soup.find('p', attrs={'class': className}).text.strip()
-	return dataText
-	
-def soupFinderTag(className, liClass):
-	for ultag in soup.find_all('ul', {'class': className}):
-		for litag in ultag.find_all('li', {'class': liClass}):
-			foundTag = litag.text
-			return foundTag
+		spans = soup.find_all('span')
+		for span in spans:
+			companies = []
+			companyName = span.find('p', {'class': 'ccaMemName'})
+			companyWeb = span.find('a', href=True)
+			companyAddr = span.find('p', {'class': 'ccaAddr'})
+			
+			if companyName:
+				print(companyName.text + companyWeb['href']+ companyAddr.text + "\n")
 
-#Function to scrape desired Data
-def scrapeData():
-	cName = soupFinder('ccaMemName')
-	wAddr = soupFinder('ccaWebAddrLk')
-	cAddr = soupFinder('ccaAddr')
-	cContact = soupFinder('ccaAddrPhone')
 
-	print(companyName)
-	
-# repName = soup.find('span', {'class': 'WebRupee'})
-# for rep in repName:
-    # print(rep.string)
-	
-badges = soup.body.find('div', attrs={'class': 'ccaMemListingContainer'})
-for span in badges.span.find_all('span'):
-    print(span.)
+data = spanScan()
+print(data)
+
+
